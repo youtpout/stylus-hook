0f4aa9c559ea6bebfff25391cb341dca396357a5
youtpout <eddyboughioul@hotmail.fr>
2024-04-18 14:51:35 +0200

    redeploy compile

--- hook/src/lib.rs
+++ hook/src/lib.rs
@@ -9,7 +9,7 @@ static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
 /// Import items from the SDK. The prelude contains common traits and macros.
 use alloc::{string::String, vec, vec::Vec};
-use alloy_primitives::{Address, U256, FixedBytes};
+use alloy_primitives::{Address, U256, FixedBytes,uint,Uint};
 use alloy_sol_types::{sol};
 use core::{borrow::BorrowMut, marker::PhantomData};
 use stylus_sdk::{
@@ -20,10 +20,6 @@ use stylus_sdk::{
     deploy::*
 };
 use std::convert::TryInto;
-
-mod hook;
-
-use hook::Counter;
 
 /// Represents the ways methods may fail.
 pub enum DeployError {
@@ -50,8 +46,7 @@ impl From<DeployError> for Vec<u8> {
 type Result<T, E = DeployError> = core::result::Result<T, E>;
 
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
+
 sol_storage! {
     #[entrypoint]
     pub struct DeployContract {
@@ -67,17 +62,20 @@ sol! {
 #[external]
 impl DeployContract {
     /// Gets the number from storage.
-    pub fn deploy_deterministic(&self) -> Result<Address,Vec<u8>> {
-        let raw : RawDeploy = RawDeploy::new();
+    pub fn deploy_deterministic(&self,code : Vec<u8>) -> Result<Address,Vec<u8>> {
+        let mut raw : RawDeploy = RawDeploy::new();
         let input = "0x0123456789012345678901234567890123456789012345678901234567890123";
         let decoded = hex::decode(input).expect("Decoding failed");
         let fix = decoded.try_into().unwrap();
         let salt = FixedBytes::<32>::new(fix);
-        raw.salt(salt);
-        //let code = Counter.self;
-        let result = raw.deploy([1001]);
-        let newAddress = result.unwrap();
-        evm::log(Deploy { contract :  newAddress });
+        raw = raw.salt(salt);
+        let data :Uint<256, 4>=Uint::<256,4>::from(0);
+        let result;
+        unsafe {
+            result = raw.deploy(code.as_ref(),data);
+        }
+        let new_address = result.clone().unwrap();
+        evm::log(Deploy { contract :  new_address });
 
         result
     }
