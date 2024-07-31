344375386ca0c3a0d9276d3eacea3daff1e59a0f
youtpout <eddyboughioul@hotmail.fr>
2024-07-31 12:47:07 +0200

    add create

--- create2/src/lib.rs
+++ create2/src/lib.rs
@@ -8,15 +8,11 @@ static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
 /// Import items from the SDK. The prelude contains common traits and macros.
 use alloc::{vec, vec::Vec};
-use alloy_primitives::{Address, FixedBytes,Uint};
-use alloy_sol_types::{sol};
+use alloy_primitives::{Address, FixedBytes, Uint, U256};
+use alloy_sol_types::sol;
 
-use stylus_sdk::{
-    evm,
-    prelude::*,
-    deploy::*
-};
 use std::convert::TryInto;
+use stylus_sdk::{deploy::*, evm, prelude::*};
 
 /// Represents the ways methods may fail.
 pub enum DeployError {
@@ -38,16 +34,13 @@ impl From<DeployError> for Vec<u8> {
     }
 }
 
-
 /// Simplifies the result type for the contract's methods.
 type Result<T, E = DeployError> = core::result::Result<T, E>;
-
-
 
 sol_storage! {
     #[entrypoint]
     pub struct DeployContract {
-       
+
     }
 }
 
@@ -59,24 +52,21 @@ sol! {
 #[external]
 impl DeployContract {
     /// Gets the number from storage.
-    pub fn deploy_deterministic(&self,code : Vec<u8>) -> Result<Address,Vec<u8>> {
-        let mut raw : RawDeploy = RawDeploy::new();
-        let input = "0x0123456789012345678901234567890123456789012345678901234567890123";
-        let decoded = hex::decode(input).expect("Decoding failed");
-        let fix = decoded.try_into().unwrap();
-        let salt = FixedBytes::<32>::new(fix);
-        raw = raw.salt(salt);
-        let data :Uint<256, 4>=Uint::<256,4>::from(0);
+    pub fn deploy_deterministic(
+        &self,
+        code: Vec<u8>,
+        salt: U256,
+    ) -> Result<Address, Vec<u8>> {
+        let mut raw: RawDeploy = RawDeploy::new();
         let result;
         unsafe {
-            result = raw.deploy(code.as_ref(),data);
+            result = raw.deploy(code.as_ref(), salt);
         }
         let new_address = result.clone().unwrap();
-        evm::log(Deploy { contract :  new_address });
+        evm::log(Deploy {
+            contract: new_address,
+        });
 
         result
     }
-
-    
-
 }
